ca00987e1953bc2e0319fecb23addc3d1b86e115
Ubuntu <ubuntu@ip-172-31-11-98.us-west-2.compute.internal>
2020-09-15 14:57:56 +0000

    added kubectl setup

--- setup_utilities.sh
+++ setup_utilities.sh
@@ -1,5 +1,28 @@
 #!/bin/bash -xe
 
-# install utilities
-apt update -y
-apt install -y unzip 
+# ---------------------------------------------------------------------------- #
+# Find package manager
+# ---------------------------------------------------------------------------- #
+if [[ -x "/usr/bin/apt-get" ]];
+  then PCK_MGR="/usr/bin/apt-get"
+elif [[ -x "/usr/bin/yum" ]];
+  then PCK_MGR="/usr/bin/yum"
+else echo "Not yum or apt exiting" 
+fi
+$PCK_MGR update -y
+
+# ---------------------------------------------------------------------------- #
+# Install utilities
+# ---------------------------------------------------------------------------- #
+if ! command -V unzip; 
+then $PCK_MGR install -y unzip 
+fi
+# ---------------------------------------------------------------------------- #
+# Install kubernetes
+# ---------------------------------------------------------------------------- #
+if ! command -V kubectl; 
+then 
+  curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+  chmod +x ./kubectl
+  mv ./kubectl /usr/local/bin/kubectl
+fi
