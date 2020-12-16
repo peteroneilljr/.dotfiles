ee1bbe51575c24d309fcd0fe8ee3239b136b07d1
Peter ONeill <33669114+peteroneilljr@users.noreply.github.com>
2020-12-16 12:44:32 -0500

    removed bad character

--- setup_utilities.sh
+++ setup_utilities.sh
@@ -89,7 +89,7 @@ fi
 if ! command -V terraform; then
   curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
   sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-  csudo apt-get update && sudo apt-get install terraform
+  sudo apt-get update && sudo apt-get install terraform
 else
   echo "Terraform already installed"
 fi
